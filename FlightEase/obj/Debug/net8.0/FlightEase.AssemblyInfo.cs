ec3011db53c4c821dff780fd148f7194da2d708c
Jarne Adam <jarne.adam@student.vives.be>
2025-02-14 13:20:43 +0100

    viewmodel en automapper

--- FlightEase/obj/Debug/net8.0/FlightEase.AssemblyInfo.cs
+++ FlightEase/obj/Debug/net8.0/FlightEase.AssemblyInfo.cs
@@ -16,7 +16,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("FlightEase")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6c8bed8f024bcb8392b5629bf37ce50dacf40ca6")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+da09d6184a864cacab000f33205ef8c4620c4582")]
 [assembly: System.Reflection.AssemblyProductAttribute("FlightEase")]
 [assembly: System.Reflection.AssemblyTitleAttribute("FlightEase")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
