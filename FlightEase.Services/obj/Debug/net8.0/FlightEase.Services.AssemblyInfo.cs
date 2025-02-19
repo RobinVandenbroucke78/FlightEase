079c599bcd4a1a372bfbcd4c6d28bf555d98fbe6
Robin Vandenbroucke <robin.vandenbroucke2@student.vives.be>
2025-02-19 15:47:31 +0100

    banner image

--- FlightEase.Services/obj/Debug/net8.0/FlightEase.Services.AssemblyInfo.cs
+++ FlightEase.Services/obj/Debug/net8.0/FlightEase.Services.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("FlightEase.Services")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a3a10c249afbdbec2862a78aab2d74a00c201ed6")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8bed5497b86e79f428bfc363b408f13025084776")]
 [assembly: System.Reflection.AssemblyProductAttribute("FlightEase.Services")]
 [assembly: System.Reflection.AssemblyTitleAttribute("FlightEase.Services")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
