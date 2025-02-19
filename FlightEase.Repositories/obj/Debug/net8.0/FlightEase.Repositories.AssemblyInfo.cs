8bed5497b86e79f428bfc363b408f13025084776
Robin Vandenbroucke <robin.vandenbroucke2@student.vives.be>
2025-02-19 15:20:07 +0100

    deleting files

--- FlightEase.Repositories/obj/Debug/net8.0/FlightEase.Repositories.AssemblyInfo.cs
+++ FlightEase.Repositories/obj/Debug/net8.0/FlightEase.Repositories.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("FlightEase.Repositories")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+008cf2fe0f5811eaca14c852d2e07621bcc7e954")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a3a10c249afbdbec2862a78aab2d74a00c201ed6")]
 [assembly: System.Reflection.AssemblyProductAttribute("FlightEase.Repositories")]
 [assembly: System.Reflection.AssemblyTitleAttribute("FlightEase.Repositories")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
