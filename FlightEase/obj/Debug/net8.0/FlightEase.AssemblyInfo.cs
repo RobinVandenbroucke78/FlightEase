4ebbde9672a55e790f11b57b3da14f011da62755
Robin Vandenbroucke <robin.vandenbroucke2@student.vives.be>
2025-02-24 16:38:07 +0100

    css loginScreen

--- FlightEase/obj/Debug/net8.0/FlightEase.AssemblyInfo.cs
+++ FlightEase/obj/Debug/net8.0/FlightEase.AssemblyInfo.cs
@@ -16,7 +16,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("FlightEase")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+625a0c810bc94c59d0f620e4df70e55742d5c670")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+34b768cae4eb75c62e827e59780440d3ec9c5040")]
 [assembly: System.Reflection.AssemblyProductAttribute("FlightEase")]
 [assembly: System.Reflection.AssemblyTitleAttribute("FlightEase")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
